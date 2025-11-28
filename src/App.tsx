763a6c933ddad72ae2fc9e2a8d558c194714d7eb
Sandra Hotsyn <sandrahotsy@gmail.com>
2025-11-28 12:30:29 +0200

    add useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import CatInfo from "./Components/CatInfo/CatInfo";
 import cats from "./cats.json";
 
 function App() {
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    console.log("Click", event);
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
   };
   return (
     <div>
+      <div> Total {count}</div>
+      <button onClick={handleClick}> click</button>
+      <button onClick={handleClick}> click</button>
       {cats.map((cat) => (
         <CatInfo
           key={cat.id}
@@ -16,7 +22,6 @@ function App() {
           aviable={cat.aviable}
         />
       ))}
-      <button onClick={handleClick}> click</button>
     </div>
   );
 }
