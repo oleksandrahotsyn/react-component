89b93d97bb2170ef0b508785d8f49186dbedca0d
Sandra Hotsyn <sandrahotsy@gmail.com>
2025-11-29 12:37:52 +0200

    add handleSearchChange

--- src/Components/TagManager/TagManager.tsx
+++ src/Components/TagManager/TagManager.tsx
@@ -5,15 +5,25 @@ const TAGS = ["Apple", "Google", "Samsung"];
 
 function TagManager() {
   const [tags, setTags] = useState<string[]>(TAGS);
+  const [search, setSearch] = useState<string>("");
 
   const handleTagManagerRemove = (tagName: string) => {
     console.log(tagName);
     setTags((prevTags) => prevTags.filter((tag) => tag != tagName));
   };
 
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  };
+
   return (
-    <div className={css.container}>
-      <div>
+    <div>
+      <input
+        placeholder="Seatch"
+        value={search}
+        onChange={handleSearchChange}
+      />
+      <div className={css.container}>
         {tags.map((tag) => (
           <span key={tag} className={css.tag}>
             {tag}{" "}
